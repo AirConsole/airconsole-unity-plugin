cc26454d0dba6be21dc9edab700dcafa4bc887c3
Alice Ruppert <alice@n-dream.com>
2017-08-29 18:15:17 +0200

    fixed pong example scene

--- Assets/AirConsole/examples/pong/scripts/Racket.cs
+++ Assets/AirConsole/examples/pong/scripts/Racket.cs
@@ -14,10 +14,10 @@ public class Racket : MonoBehaviour {
 		if (col.gameObject.GetComponent<Rigidbody2D> () != null) {
 
 			float hitPos = (col.transform.position.y - transform.position.y) / (GetComponent<Collider2D> ().bounds.size.y / 2);
-			float hitDir = -1f;
+			float hitDir = 1f;
 
 			if (col.relativeVelocity.x > 0) {
-				hitDir = 1f;
+				hitDir = -1f;
 			}
 
 			Vector2 dir = new Vector2 (hitDir, hitPos).normalized;
