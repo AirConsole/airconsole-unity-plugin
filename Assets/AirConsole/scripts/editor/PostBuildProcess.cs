60a8f52dd9e8f85af46562525a4fdadc00c11357
Daniel <daniel@n-dream.com>
2020-05-26 17:17:54 +0200

    Fixed C# indentation

--- Assets/AirConsole/scripts/editor/PostBuildProcess.cs
+++ Assets/AirConsole/scripts/editor/PostBuildProcess.cs
@@ -21,17 +21,17 @@ namespace NDream.AirConsole.Editor {
 
 				// Check if game.json already exists
 				if (File.Exists (pathToBuiltProject + "/Build/game.json")) {
-					File.Delete (pathToBuiltProject + "/Build/game.json");
+				File.Delete (pathToBuiltProject + "/Build/game.json");
 				}
 
-                string configuration_file_path = pathToBuiltProject + "/Build/" + Path.GetFileName (pathToBuiltProject) + ".json";
+				string configuration_file_path = pathToBuiltProject + "/Build/" + Path.GetFileName (pathToBuiltProject) + ".json";
 
-                // Rename JSON configuration to game.json (Only for Unity versions < 2020.x)
-                // See https://forum.unity.com/threads/changes-to-the-webgl-loader-and-templates-introduced-in-unity-2020-1.817698/
-                // for details, the build config is no longer stored in a JSON file but embedded in the HTML
-                if (File.Exists (configuration_file_path)) {
-  					File.Move (configuration_file_path, pathToBuiltProject + "/Build/game.json");
-                }
+				// Rename JSON configuration to game.json (Only for Unity versions < 2020.x)
+				// See https://forum.unity.com/threads/changes-to-the-webgl-loader-and-templates-introduced-in-unity-2020-1.817698/
+				// for details, the build config is no longer stored in a JSON file but embedded in the HTML
+				if (File.Exists (configuration_file_path)) {
+					File.Move (configuration_file_path, pathToBuiltProject + "/Build/game.json");
+				}
 
 				// Save last port path
 				EditorPrefs.SetString ("airconsolePortPath", pathToBuiltProject);
