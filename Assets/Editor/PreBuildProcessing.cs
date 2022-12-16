a60978d05a520c0887849955712a58d52b54166b
Daniel <daniel@n-dream.com>
2022-12-16 15:40:37 +0100

    Improved comment

--- Assets/Editor/PreBuildProcessing.cs
+++ Assets/Editor/PreBuildProcessing.cs
@@ -9,9 +9,10 @@ public class PreBuildProcessing : IPreprocessBuildWithReport {
     public void OnPreprocessBuild(BuildReport report) {
         Debug.Log("Used Python path: " + System.Environment.GetEnvironmentVariable("EMSDK_PYTHON"));
 
-        // In case you get a Build exception from Unity (System.ComponentModel.Win32Exception (2):
-        // No such file or directory), make sure that the correct Python version is installed and
-        // can be found by Unity during the Build process.
+        // In case you get a Build exception from Unity such as:
+        //   System.ComponentModel.Win32Exception (2): No such file or directory)
+        // Make sure that the correct Python version is installed and can be found by Unity during
+        // the Build process.
 
         // If you need to set the Python path manually you can use the code below, uncomment it and
         // set "python_path" to the the Python 3 (Or Python 2 for old Unity versions) path
