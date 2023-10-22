341358010bc82576f080ff6a338fb9e5e041b08e
marc-n-dream <143824611+marc-n-dream@users.noreply.github.com>
2023-10-22 18:08:26 +0200

    f: Update plugin version to v2.13

--- Assets/AirConsole/scripts/Settings.cs
+++ Assets/AirConsole/scripts/Settings.cs
@@ -4,7 +4,7 @@ using System.Collections;
 namespace NDream.AirConsole {
 	public static class Settings {
 
-		public const string VERSION = "2.12";
+		public const string VERSION = "2.13";
 		public const string AIRCONSOLE_BASE_URL = "https://www.airconsole.com/";
 		public const string AIRCONSOLE_DEV_URL = "http://http.airconsole.com/";
 		public const string AIRCONSOLE_PROFILE_PICTURE_URL = "https://www.airconsole.com/api/profile-picture?uid=";
