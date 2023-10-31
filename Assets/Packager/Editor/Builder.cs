500fa9e429c376ac2804f30dc38085c379eaa006
marc-n-dream <143824611+marc-n-dream@users.noreply.github.com>
2023-10-31 10:40:18 +0100

    f: Prepare release pipeline

---
+++ Assets/Packager/Editor/Builder.cs
@@ -0,0 +1,17 @@
+using UnityEditor;
+using UnityEngine;
+
+namespace NDream.Unity
+{
+    public class Builder
+    {
+        public static void BuildAndroid()
+        {
+          Debug.LogError("Building Android");
+        }
+
+        public static void BuildWebGL(){
+          Debug.LogError("Building WebGL");
+        }
+    }
+}
