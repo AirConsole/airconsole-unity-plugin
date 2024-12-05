d2baed4d0035300416e6b0b82e8c4395b8082597
Martin Hodler <martin@n-dream.com>
2024-12-05 15:48:10 +0100

    F**: Add pre-build processing step adding attributes to AndroidManifest for Unity 6 and cleanup usings.

--- Assets/AirConsole/scripts/editor/PreBuildProcessing.cs
+++ Assets/AirConsole/scripts/editor/PreBuildProcessing.cs
@@ -1,8 +1,7 @@
 #if UNITY_EDITOR
-using System;
 using System.IO;
 using System.Linq;
-using NDream.AirConsole;
+using System.Xml.Linq;
 using UnityEditor;
 using UnityEditor.Build;
 using UnityEditor.Build.Reporting;
@@ -11,6 +10,14 @@ using UnityEngine;
 public class PreBuildProcessing : IPreprocessBuildWithReport {
     public int callbackOrder => 1;
 
+    private const string ANDROID_MANIFEST_PATH = "Assets/Plugins/Android/AndroidManifest.xml";
+    
+    private const string UNITY6_ANDROID_ACTIVITY_NAME = "com.unity3d.player.UnityPlayerGameActivity";
+    private const string UNITY_ANDROID_ACTIVITY_NAME = "com.unity3d.player.UnityPlayerActivity";
+
+    private const string UNITY6_ANDROID_ACTIVITY_THEME = "@style/BaseUnityGameActivityTheme";
+    
+    
     public void OnPreprocessBuild(BuildReport report) {
         CheckWebGLSetup();
             
@@ -26,13 +33,78 @@ public class PreBuildProcessing : IPreprocessBuildWithReport {
 #if !UNITY_2020_1_OR_NEWER && UNITY_EDITOR_OSX
         System.Environment.SetEnvironmentVariable("EMSDK_PYTHON", Settings.Python2Path);
 #endif
+        
+#if UNITY_ANDROID
+        ValidateAndroidManifest();
+#endif
+    }
+
+
+    private static void ValidateAndroidManifest()
+    {
+        string disabledManifestPath = Path.GetFullPath($"{ANDROID_MANIFEST_PATH}.DISABLED");
+        string manifestPath = Path.GetFullPath(ANDROID_MANIFEST_PATH);
+        
+        if (File.Exists(disabledManifestPath))
+        {
+            File.Move(disabledManifestPath, manifestPath);
+        }
+        
+        if (File.Exists(manifestPath))
+        {
+            XDocument manifest = XDocument.Load(manifestPath);
+
+            if (manifest.Root == null) return;
+            
+            XElement[] applicationElements = manifest.Root.Elements("application").ToArray();
+            if (!applicationElements.Any()) return;
+
+            XElement[] activityElements = applicationElements.Elements("activity").ToArray();
+            if (!activityElements.Any()) return;
+
+            XName nameAttribute = XName.Get("name", "http://schemas.android.com/apk/res/android");
+            XName themeAttribute = XName.Get("theme", "http://schemas.android.com/apk/res/android");
+
+            foreach (XElement activityElement in activityElements)
+            {
+                XAttribute name = activityElement.Attribute(nameAttribute);
+                if (name == null)
+                    continue;
+                
+                string activityName = name.Value;
+
+                if (activityName == UNITY6_ANDROID_ACTIVITY_NAME || activityName == UNITY_ANDROID_ACTIVITY_NAME)
+                {
+                    #if UNITY_6000_0_OR_NEWER
+                    name.Value = UNITY6_ANDROID_ACTIVITY_NAME;
+                    
+                    XAttribute theme = activityElement.Attribute(themeAttribute);
+                    if (theme == null)
+                    {
+                        activityElement.SetAttributeValue(themeAttribute, UNITY6_ANDROID_ACTIVITY_THEME);
+                    }
+                    #else
+                    name.Value = UNITY_ANDROID_ACTIVITY_NAME;
+                    #endif
+                    
+                    break;
+                }
+            }
+            
+            manifest.Save(manifestPath);
+        }
+        else
+        {
+            throw new UnityException(
+                $"{ANDROID_MANIFEST_PATH} does not exist. AirConsole requires certain permissions and configuration to correctly run on Android.");
+        }
     }
 
     private static void CheckWebGLSetup() {
 #if UNITY_WEBGL
         if (string.IsNullOrEmpty(PlayerSettings.WebGL.template)) {
             EditorUtility.DisplayDialog("Error", "No WebGL Template configured", "Cancel");
-            throw new Exception("WebGL template not configured");
+            throw new UnityException("WebGL template not configured");
         }
 
         if (Directory.Exists(GetWebGLTemplateDirectory())) {
@@ -40,13 +112,13 @@ public class PreBuildProcessing : IPreprocessBuildWithReport {
             if (!Directory.GetFiles(templatePath).Any(filename => filename.EndsWith("controller.html"))) {
                 EditorUtility.DisplayDialog("Error",
                     "The controller has not yet been generated. Please execute the game at least once in play mode.", "Cancel");
-                throw new Exception("Controller missing in WebGL template location.");
+                throw new UnityException("Controller missing in WebGL template location.");
             }
 
             if (!Directory.GetFiles(templatePath).Any(filename => filename.EndsWith("airconsole-unity-plugin.js"))) {
                 EditorUtility.DisplayDialog("Error", "airconsole-unity-plugin missing. Please set up your airconsole plugin again",
                     "Cancel");
-                throw new Exception("Unity template incomplete");
+                throw new UnityException("Unity template incomplete");
             }
         }
 #endif
