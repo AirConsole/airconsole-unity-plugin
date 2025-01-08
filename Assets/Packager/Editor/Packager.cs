920a8a5493d7362b89a70cfc1062acba805b4254
marc-n-dream <143824611+marc-n-dream@users.noreply.github.com>
2025-01-08 17:04:41 +0100

    e: Fix packager

--- Assets/Packager/Editor/Packager.cs
+++ Assets/Packager/Editor/Packager.cs
@@ -1,4 +1,6 @@
 #region
+
+using System.Collections.Generic;
 using NDream.AirConsole;
 using System.Diagnostics;
 using System.IO;
@@ -12,8 +14,22 @@ using Debug = UnityEngine.Debug;
 
 namespace NDream.Unity
 {
-    public class Packager
+    public static class Packager
     {
+
+        [MenuItem("Tools/AirConsole/Marc is a Monkey")]
+        public static void FixMarcIsAMonkey() {
+            EditorApplication.UnlockReloadAssemblies();
+            
+            string packageCache = Path.GetFullPath(Path.Combine(Application.dataPath, "..", "Library", "PackageCache"));
+            string webviewPackagePath = Directory.GetDirectories(packageCache).FirstOrDefault(d => d.Contains("com.airconsole.unity-webview"));
+            webviewPackagePath = Path.Combine(webviewPackagePath, "Assets");
+            
+            string targetPath = Path.GetFullPath(Path.Combine(Application.dataPath, "AirConsole", "unity-webview"));
+            
+            MoveSubDirectories(webviewPackagePath, targetPath);
+        }
+        
         [MenuItem("Tools/AirConsole/Package Plugin")]
         public static void Export()
         {
@@ -21,7 +37,7 @@ namespace NDream.Unity
             Debug.Log($"Exporting to {outputPath}");
 
             string packageCache = Path.GetFullPath(Path.Combine(Application.dataPath, "..", "Library", "PackageCache"));
-            string webviewPackagePath = Directory.GetDirectories(packageCache).FirstOrDefault(d => d.Contains("com.airconsole.webview"));
+            string webviewPackagePath = Directory.GetDirectories(packageCache).FirstOrDefault(d => d.Contains("com.airconsole.unity-webview"));
 
             if(!Directory.Exists(webviewPackagePath))
             {
@@ -30,16 +46,27 @@ namespace NDream.Unity
                 return;
             }
             
+            string webviewPackagePathAssets = Path.Combine(webviewPackagePath, "Assets");
+            
             EditorApplication.LockReloadAssemblies();
             string targetPath = Path.GetFullPath(Path.Combine(Application.dataPath, "AirConsole", "unity-webview"));
-            if(Directory.Exists(targetPath)) Directory.CreateDirectory(targetPath);
-            Directory.Move(webviewPackagePath, targetPath);
+            if (!Directory.Exists(targetPath)) Directory.CreateDirectory(targetPath);
+            
+            MoveSubDirectories(webviewPackagePathAssets, targetPath);
+            List<string> filesToDelete = new();
+            CopyFiles(webviewPackagePath, targetPath, filesToDelete);
+            // Directory.Move(webviewPackagePath, targetPath);
             AssetDatabase.Refresh();
             
             AssetDatabase.ExportPackage(new[] { "Assets/AirConsole", "Assets/Plugins", "Assets/WebGLTemplates" },
-                                        outputPath, ExportPackageOptions.Recurse | ExportPackageOptions.IncludeDependencies);
-           
-            Directory.Move(targetPath, webviewPackagePath); 
+                                        outputPath, ExportPackageOptions.Recurse);
+          
+            foreach (string file in filesToDelete) {
+                File.Delete(file);
+            }
+            MoveSubDirectories(targetPath,webviewPackagePathAssets);
+            // Directory.Move(targetPath, webviewPackagePath);
+            DeleteUnityProjectDirectory(targetPath);
             AssetDatabase.Refresh();
             EditorApplication.UnlockReloadAssemblies();
             Debug.ClearDeveloperConsole();
@@ -65,12 +92,41 @@ namespace NDream.Unity
             Application.OpenURL("file://" + Path.GetDirectoryName(Path.Combine(Application.dataPath, "..", outputPath)));
         }
 
+        private static void DeleteUnityProjectDirectory(string targetPath) {
+            if (!Directory.Exists(targetPath)) {
+                return;
+            }
+
+            Directory.Delete(targetPath, true);
+            File.Delete(targetPath + ".meta");
+        }
+
         private static void DeleteOldUnityPackages(string outputPath, string newVersion) {
             string[] files = Directory.GetFiles(Path.GetDirectoryName(outputPath), "airconsole-unity-plugin-*.*");
             foreach (string file in files) {
                 if (!file.Contains(newVersion)) {
                     File.Delete(file);
                 }
+            }
+        }
+
+        private static void MoveSubDirectories(string sourcePath, string targetPath) {
+            IEnumerable<string> sources = Directory.GetDirectories(sourcePath);
+            sources = sources.Concat(Directory.GetFiles(sourcePath));
+            Debug.Log($"Files to copy:\n{string.Join("\n", sources)}");
+            foreach (string source in sources) {
+                string target = Path.Combine(targetPath, Path.GetFileName(source));
+                Directory.Move(source, target);
+            }
+        }
+
+        private static void CopyFiles(string sourcePath, string targetPath, List<string> copiedFiles, string filter = "*.asmdef*") {
+            IEnumerable<string> sources = Directory.GetFiles(sourcePath, filter);
+            Debug.Log($"Files to copy:\n{string.Join("\n", sources)}");
+            foreach (string source in sources) {
+                string target = Path.Combine(targetPath, Path.GetFileName(source));
+                File.Copy(source, target, true);
+                copiedFiles.Add(target);
             }
         }
     }
