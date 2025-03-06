85b88d05bc91f8f7258f503c078c261c6e85e59b
marc-n-dream <marc@n-dream.com>
2025-03-06 09:44:44 +0100

    a: Automatic code style update for scripts/editor

--- Assets/AirConsole/scripts/editor/WebListener.cs
+++ Assets/AirConsole/scripts/editor/WebListener.cs
@@ -7,149 +7,148 @@ using System.Text;
 using UnityEngine;
 
 namespace NDream.AirConsole.Editor {
-	public class WebListener {
+    public class WebListener {
+        // private vars
+        private HttpListener listener;
+        private string startUpPath;
+        private string prefix;
+        private HttpListenerContext request;
+        private Thread t;
 
-		// private vars
-		private HttpListener listener;
-		private string startUpPath;
-		private string prefix;
-		private HttpListenerContext request;
-		private Thread t;
+        public void SetPath(string path) {
+            startUpPath = path;
+        }
 
-		public void SetPath (string path) {
-			startUpPath = path;
-		}
+        public void Start() {
+            if (listener == null) {
+                listener = new HttpListener();
+            }
 
-		public void Start () {
+            prefix = string.Format("http://*:{0}/", Settings.webServerPort.ToString());
 
-			if (listener == null) {
-				listener = new HttpListener ();
-			}
+            if (!listener.IsListening) {
+                listener.Start();
 
-			prefix = string.Format ("http://*:{0}/", Settings.webServerPort.ToString ());
+                if (!listener.Prefixes.Contains(prefix)) {
+                    listener.Prefixes.Add(prefix);
+                }
 
-			if (!listener.IsListening) {
-				listener.Start ();
+                if (t != null && t.IsAlive) {
+                    t.Abort();
+                }
 
-				if (!listener.Prefixes.Contains (prefix)) {
-					listener.Prefixes.Add (prefix);
-				}
+                t = new Thread(new ThreadStart(ClientListener));
+                t.Start();
+            }
+        }
 
-				if (t != null && t.IsAlive) {
-					t.Abort ();
-				}
+        public bool IsRunning() {
+            if (listener != null) {
+                return listener.IsListening;
+            }
 
-				t = new Thread (new ThreadStart (ClientListener));
-				t.Start ();
-			}
-		}
+            return false;
+        }
 
-		public bool IsRunning () {
-			if (listener != null) {
-				return listener.IsListening;
-			}
-			return false;
-		}
+        public void ClientListener() {
+            while (true) {
+                try {
+                    request = listener.GetContext();
+                    ThreadPool.QueueUserWorkItem(ProcessRequest, request);
+                } catch (ThreadAbortException) {
+                    // ThreadAbortException is thrown when the webserver gets stopped/restarted
+                } catch (Exception e) {
+                    if (Settings.debug.error) {
+                        Debug.LogError(e.ToString());
+                    }
+                }
+            }
+        }
 
-		public void ClientListener () {
-			while (true) {
-				try {
-					request = listener.GetContext ();
-					ThreadPool.QueueUserWorkItem (ProcessRequest, request);
-				} catch (ThreadAbortException) {
-					// ThreadAbortException is thrown when the webserver gets stopped/restarted
-				} catch (Exception e) {
-					if (Settings.debug.error) {
-						Debug.LogError (e.ToString ());
-					}
-				}
-			}
-		}
+        public void ProcessRequest(object listenerContext) {
+            try {
+                HttpListenerContext context = (HttpListenerContext)listenerContext;
+                string rawUrl = context.Request.RawUrl;
 
-		public void ProcessRequest (object listenerContext) {
-			try {
-				var context = (HttpListenerContext)listenerContext;
-				string rawUrl = context.Request.RawUrl;
+                // conditions if editor version gets called
+                if (startUpPath.Contains(Settings.WEBTEMPLATE_PATH)) {
+                    // remove query parameters
+                    rawUrl = rawUrl.Split('?')[0];
+                    // translate screen.html to index.html
+                    rawUrl = rawUrl.Replace("screen.html", "index.html");
+                }
 
-				// conditions if editor version gets called
-				if (startUpPath.Contains (Settings.WEBTEMPLATE_PATH)) {
-					// remove query parameters
-					rawUrl = rawUrl.Split ('?') [0];
-					// translate screen.html to index.html
-					rawUrl = rawUrl.Replace ("screen.html", "index.html");
-				}
+                string filename = Path.GetFileName(rawUrl);
+                string path = startUpPath + Uri.UnescapeDataString(rawUrl);
+                ;
 
-				string filename = Path.GetFileName (rawUrl);
-				string path = startUpPath + System.Uri.UnescapeDataString(rawUrl);;
+                byte[] msg;
 
-				byte[] msg;
+                if (!File.Exists(path)) {
+                    context.Response.StatusCode = (int)HttpStatusCode.NotFound;
+                    msg = Encoding.UTF8.GetBytes(
+                        "<html><head><title>AirConsole Error</title></head><body><h1>AirWebserver can't find resources!</h1></body></html>");
+                } else {
+                    context.Response.StatusCode = (int)HttpStatusCode.OK;
+                    msg = File.ReadAllBytes(path);
+                    context.Response.ContentType = ReturnMIMEType(Path.GetExtension(filename));
+                }
 
-				if (!File.Exists (path)) {
-					context.Response.StatusCode = (int)HttpStatusCode.NotFound;
-					msg = Encoding.UTF8.GetBytes ("<html><head><title>AirConsole Error</title></head><body><h1>AirWebserver can't find resources!</h1></body></html>");
+                context.Response.ContentLength64 = msg.Length;
 
-				} else {
-					context.Response.StatusCode = (int)HttpStatusCode.OK;
-					msg = File.ReadAllBytes (path);
-					context.Response.ContentType = ReturnMIMEType (Path.GetExtension (filename));
-				}
+                using (Stream s = context.Response.OutputStream) {
+                    s.Write(msg, 0, msg.Length);
+                }
+            } catch (Exception e) {
+                if (Settings.debug.error) {
+                    if (e.Message != "Write failure") {
+                        Debug.LogError(e.Message);
+                    }
+                }
+            }
+        }
 
-				context.Response.ContentLength64 = msg.Length;
+        public string ReturnMIMEType(string filename) {
+            switch (filename) {
+                case ".txt":
+                    return "text/plain";
+                case ".gif":
+                    return "image/gif";
+                case ".png":
+                    return "image/png";
+                case ".jpg":
+                case "jpeg":
+                    return "image/jpeg";
+                case ".bmp":
+                    return "image/bmp";
+                case ".svg":
+                    return "image/svg+xml";
+                case ".wav":
+                    return "audio/wav";
+                case ".mp3":
+                    return "audio/mp3";
+                case ".html":
+                    return "text/html";
+                case ".htm":
+                    return "text/html";
+                case ".css":
+                    return "text/css";
+                case ".js":
+                    return "application/javascript";
+                default:
+                    return "application/octet-stream";
+            }
+        }
 
-				using (Stream s = context.Response.OutputStream) {
-					s.Write (msg, 0, msg.Length);
-				}
-			} catch (Exception e) {
-				if (Settings.debug.error) {
-					if (e.Message != "Write failure") {
-						Debug.LogError (e.Message);
-					}
-				}
-			}
-		}
+        public void Stop() {
+            t.Abort();
+            listener.Stop();
+        }
 
-		public string ReturnMIMEType (string filename) {
-
-			switch (filename) {
-			case ".txt":
-				return "text/plain";
-			case ".gif":
-				return "image/gif";
-			case ".png":
-				return "image/png";
-			case ".jpg":
-			case "jpeg":
-				return "image/jpeg";
-			case ".bmp":
-				return "image/bmp";
-			case ".svg":
-				return "image/svg+xml";
-			case ".wav":
-				return "audio/wav";
-			case ".mp3":
-				return "audio/mp3";
-			case ".html":
-				return "text/html";
-			case ".htm":
-				return "text/html";
-			case ".css":
-				return "text/css";
-			case ".js":
-				return "application/javascript";
-			default:
-				return "application/octet-stream";
-			}
-		}
-
-		public void Stop () {
-			t.Abort ();
-			listener.Stop ();
-		}
-
-		public void Restart () {
-			Stop ();
-			Start ();
-		}
-	}
+        public void Restart() {
+            Stop();
+            Start();
+        }
+    }
 }
 #endif
