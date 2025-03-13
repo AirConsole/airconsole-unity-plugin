b241894c1b67e51b14d47abfe6bab4ffa793bd2b
marc-n-dream <marc@n-dream.com>
2025-03-13 17:08:59 +0100

    f: Add AssemblyInfo
    This adds a InternalsVisibleTo declaration to make internal functions accessible to the editor and runtime tests and runtime internals to the editor

---
+++ Assets/AirConsole/scripts/Runtime/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("AirConsole.Editor")]
+[assembly: InternalsVisibleTo("AirConsole.EditMode.Tests")]
+[assembly: InternalsVisibleTo("AirConsole.PlayMode.Tests")]
+
+namespace NDream.AirConsole {
+    public class AssemblyInfo { }
+}
