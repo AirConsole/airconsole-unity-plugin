b8f9d88ef22bcc8684a2f9df83c557abd58122bd
marc-n-dream <marc@n-dream.com>
2025-04-15 17:10:55 +0200

    f: Abstract access to the Unity Android Activity
    This ensures better compatibility with the 2 distinct game activities on
    Unity 6

---
+++ Assets/AirConsole/scripts/Runtime/Plugin/Android/UnityAndroidObjectProvider.cs
@@ -0,0 +1,35 @@
+#if !DISABLE_AIRCONSOLE && UNITY_ANDROID
+#if UNITY_ANDROID && !UNITY_EDITOR
+#define AIRCONSOLE_ANDROID
+#endif
+
+namespace NDream.AirConsole.Android.Plugin {
+    using UnityEngine;
+
+    internal abstract class UnityAndroidObjectProvider {
+        internal static AndroidJavaObject GetUnityContext() {
+#if !AIRCONSOLE_ANDROID
+            throw new UnityException("UnityAndroidObjectProvider is only supported on Unity Android builds.");
+#endif
+#if UNITY_6000_0_OR_NEWER
+            return UnityEngine.Android.AndroidApplication.currentContext;
+#endif
+
+            AndroidJavaClass unityPlayer = new("com.unity3d.player.UnityPlayer");
+            return unityPlayer.GetStatic<AndroidJavaObject>("currentActivity");
+        }
+
+        internal static AndroidJavaObject GetUnityActivity() {
+#if !AIRCONSOLE_ANDROID
+            throw new UnityException("UnityAndroidObjectProvider is only supported on Unity Android builds.");
+#endif
+#if UNITY_6000_0_OR_NEWER
+            return UnityEngine.Android.AndroidApplication.currentActivity;
+#endif
+
+            AndroidJavaClass unityPlayer = new("com.unity3d.player.UnityPlayer");
+            return unityPlayer.GetStatic<AndroidJavaObject>("currentActivity");
+        }
+    }
+}
+#endif
