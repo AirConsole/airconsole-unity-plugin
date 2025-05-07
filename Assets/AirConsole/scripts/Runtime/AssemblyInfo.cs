91c31c5f9167c8d732d0f7a565ffc8537f1bb8bf
marc-n-dream <m143824611+marc-n-dream@users.noreply.github.com>
2025-05-07 12:15:15 +0200

    r: Update assembly definitions

--- Assets/AirConsole/scripts/Runtime/AssemblyInfo.cs
+++ Assets/AirConsole/scripts/Runtime/AssemblyInfo.cs
@@ -2,6 +2,7 @@ using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("AirConsole.Editor")]
 [assembly: InternalsVisibleTo("AirConsole.EditMode.Tests")]
+[assembly: InternalsVisibleTo("AirConsole.Editor.Tests")]
 [assembly: InternalsVisibleTo("AirConsole.PlayMode.Tests")]
 
 namespace NDream.AirConsole {
