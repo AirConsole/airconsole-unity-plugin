41dab7bafc23ea1c4a6b66123ad23bb45ce86e14
marc-n-dream <marc@n-dream.com>
2025-08-13 16:41:44 +0200

    . R: Improve version not supported handling
    - It no longer deleletes but reports.
    - It makes builds fail.

--- Assets/AirConsole/scripts/SupportCheck/UnityVersionNotSupported.cs
+++ Assets/AirConsole/scripts/SupportCheck/UnityVersionNotSupported.cs
@@ -1,25 +1,34 @@
-using System.IO;
 #if !DISABLE_AIRCONSOLE
 namespace NDream.AirConsole.Support {
     using UnityEditor;
     using UnityEngine;
+    using UnityEditor.Build;
+    using UnityEditor.Build.Reporting;
 
-    [InitializeOnLoad]
-    internal class UnityVersionNotSupported {
-        static UnityVersionNotSupported() {
 #if !UNITY_2022_3_OR_NEWER
-            string message = $"AirConsole requires Unity 2022.3 or newer. Deleting AirConsole directory again.";
+    internal class UnityVersionNotSupported : MonoBehaviour, IPreprocessBuildWithReport {
+        [InitializeOnLoadMethod]
+        private static void TriggerNotSupportedVersion() {
+            string message = $"AirConsole requires Unity 2022.3 or newer. Please upgrade the project.";
             EditorUtility.DisplayDialog("Not supported", message, "Ok");
             Debug.LogError(message);
-            
-            
-            EditorApplication.LockReloadAssemblies();
-            AssetDatabase.DeleteAsset("Assets/AirConsole");
-            Directory.Delete(Path.Combine(Application.dataPath, "AirConsole"));
-            EditorApplication.UnlockReloadAssemblies();
-            AssetDatabase.Refresh();
-#endif
+        }
+
+        private void Awake() {
+            TriggerNotSupportedVersion();
+        }
+        private void Start() {
+            EditorApplication.isPlaying = false;
+        }
+
+        public int callbackOrder {
+            get => 1;
+        }
+
+        public void OnPreprocessBuild(BuildReport report) {
+            throw new BuildFailedException($"AirConsole requires Unity 2022.3 or newer but found {Application.unityVersion}.");
         }
     }
+#endif
 }
 #endif
