971422b9c1d3960469ec2e4bf971b3fba51a9ed0
marc-n-dream <marc@n-dream.com>
2025-12-11 10:58:11 +0100

    F: Integrate android audio focus capabilities and webview reload improvements

---
+++ Assets/AirConsole/examples/basic/AudioManager.cs
@@ -0,0 +1,34 @@
+#if !DISABLE_AIRCONSOLE
+namespace NDream.AirConsole.Examples {
+    using NDream.AirConsole;
+    using UnityEngine;
+
+    /// <summary>
+    /// Example Audio Player that starts playing when the game is ready and stops when the game ends.
+    /// It also listens to volume changes and adjusts the audio accordingly.
+    /// </summary>
+    public class AudioManager : MonoBehaviour {
+        // We are using a Platform Script Defines here to avoid extra code to handle non-Android runtime platforms.
+#if UNITY_ANDROID && !UNITY_EDITOR 
+        private void Awake() {
+            // Until OnReady is called, we don't want any audio from Unity playing as the webview Player Lobby overlay will be shown.
+            AudioListener.pause = true;
+
+            AirConsole.instance.OnGameAudioFocusChanged += HandleGameAudioFocusChange;
+        }
+
+        private void OnDestroy() {
+            if (AirConsole.instance) {
+                AirConsole.instance.OnGameAudioFocusChanged -= HandleGameAudioFocusChange;
+            }
+        }
+
+        private void HandleGameAudioFocusChange(bool hasAudioFocus, float newMaximumVolume) {
+            AirConsoleLogger.Log(() => $"HandleGameAudioFocusChange({hasAudioFocus},{newMaximumVolume})");
+            AudioListener.volume = newMaximumVolume;
+            AudioListener.pause = !hasAudioFocus;
+        }
+#endif
+    }
+}
+#endif
--- Assets/AirConsole/scripts/Editor/BuildAutomation/AndroidGradleProcessor.cs
+++ Assets/AirConsole/scripts/Editor/BuildAutomation/AndroidGradleProcessor.cs
@@ -4,6 +4,7 @@ namespace NDream.AirConsole.Editor {
     using System.Collections.Generic;
     using System.IO;
     using System.Linq;
+    using System.Text.RegularExpressions;
 
     internal abstract class AndroidGradleProcessor {
         private const string PROGUARD_CLASSMEMBERS
@@ -11,9 +12,66 @@ namespace NDream.AirConsole.Editor {
 
         internal static void Execute(string basePath) {
             UpdateMainGradleProperties(Path.GetFullPath(Path.Combine(basePath, "..")), "gradle.properties");
-            UpdateMainGradleTemplate(Path.GetFullPath(basePath), "build.gradle");
+
+            UpdateLibraryGradleTemplate(Path.GetFullPath(basePath), "build.gradle");
             UpdateProGuard(Path.GetFullPath(basePath), "proguard-unity.txt");
             AirConsoleLogger.LogDevelopment(() => "Updated gradle files for AirConsole Android build");
+        }
+
+        private static void UpdateMainGradleTemplate(string basePath, string buildGradle) {
+            string[] lines = File.ReadAllLines(Path.Combine(basePath, buildGradle));
+
+            Regex applicationVersionExtractor
+                = new(@"id 'com.android.application' version '(?<Major>\d+)\.(?<Minor>\d+)\.(?<Build>\d+)' apply false");
+            Regex libraryVersionVersionExtractor
+                = new(@"id 'com.android.library' version '(?<Major>\d+)\.(?<Minor>\d+)\.(?<Build>\d+)' apply false");
+            bool updated = false;
+            for (int i = 0; i < lines.Length; i++) {
+                // regex to check the used gradle version
+                Match match = applicationVersionExtractor.Match(lines[i]);
+                if (match.Success) {
+                    if (int.Parse(match.Groups["Major"].Value) < 8
+                        || int.Parse(match.Groups["Minor"].Value) < 1
+                        || int.Parse(match.Groups["Build"].Value) < 1) {
+                        lines[i] = "id 'com.android.application' version '8.1.1' apply false";
+                        updated = true;
+                    }
+                } else {
+                    match = libraryVersionVersionExtractor.Match(lines[i]);
+                    if (match.Success) {
+                        if (int.Parse(match.Groups["Major"].Value) < 8
+                            || int.Parse(match.Groups["Minor"].Value) < 1
+                            || int.Parse(match.Groups["Build"].Value) < 1) {
+                            lines[i] = "id 'com.android.library' version '8.1.1' apply false";
+                            updated = true;
+                        }
+                    }
+                }
+            }
+
+            if (updated) {
+                File.WriteAllLines(Path.Combine(basePath, buildGradle), lines);
+                AirConsoleLogger.LogDevelopment(() => "Updated gradle main template");
+            }
+        }
+
+        private static void UpdateGradleWrapperVersion(string basePath, string gradleWrapperProperties) {
+            string[] lines = File.ReadAllLines(Path.Combine(basePath, gradleWrapperProperties));
+            Regex versionExtractor
+                = new(@"^distributionUrl=.*/gradle-(?<Major>\d+)\.(?<Minor>\d+)\.(?<Build>\d+)-bin.zip$");
+            for (int i = 0; i < lines.Length; i++) {
+                // regex to check the used gradle version
+                Match match = versionExtractor.Match(lines[i]);
+                if (lines[i].StartsWith("distributionUrl=") && match.Success
+                    && (int.Parse(match.Groups["Major"].Value) < 8
+                        || int.Parse(match.Groups["Minor"].Value) < 1
+                        || int.Parse(match.Groups["Build"].Value) < 1)) {
+                    lines[i] = "distributionUrl=https\\://services.gradle.org/distributions/gradle-8.1.1-bin.zip";
+                    File.WriteAllLines(Path.Combine(basePath, gradleWrapperProperties), lines);
+                    AirConsoleLogger.LogDevelopment(() => $"Updated gradle wrapper to {lines[i]}");
+                    return;
+                }
+            }
         }
 
         private static void UpdateProGuard(string basePath, string proguardUnityTxt) {
@@ -27,7 +85,7 @@ namespace NDream.AirConsole.Editor {
             File.WriteAllText(filePath, fileText);
         }
 
-        private static void UpdateMainGradleTemplate(string basePath, string gradleTemplateName) {
+        private static void UpdateLibraryGradleTemplate(string basePath, string gradleTemplateName) {
             string gradleTemplatePath = Path.Combine(basePath, gradleTemplateName);
             string[] initialLines = File.ReadAllText(gradleTemplatePath)
                 .Replace("\r\n", "\n")
@@ -70,6 +128,10 @@ namespace NDream.AirConsole.Editor {
 
             if (!lines.Contains("android.useAndroidX=true")) {
                 lines = "# AirConsole\nandroid.useAndroidX=true\n\n# Unity provided\n" + lines;
+            }
+
+            if (!lines.Contains("android.suppressUnsupportedCompileSdk")) {
+                lines += "\nandroid.suppressUnsupportedCompileSdk=35,34";
             }
 
             if (lines != initialLines) {
